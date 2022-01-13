fe7bb8b17a1eb88716028b756a74f3095f32102b
Matt Eilar <meilar@gmail.com>
2022-01-13 14:15:10 -0800

    BUGFIX: change jQuery selectors and form id
    Co-authored-by: Matt Eilar <meilar@gmail.com>
    Co-authored-by: William Jameson <williamjameson90@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$(document).ready()
+$(document).ready(function() {
   $("form#input-form").submit(function(event) {
     event.preventDefault();
     const side1 = $("input#side-1").val();
     const side2 = $("input#side-2").val();
     const side3 = $("input#side-3").val();
 
-    if(side1 === side2 === side3) {
+    if(side1 === side2 && side2 === side3) {
       $("span#triangle-id").text("An Equilateral triangle!");
      } else if(side1 === side2 || side2 === side3 || side3 === side1) {
       $("span#triangle-id").text("An Isosceles triangle");
@@ -14,4 +14,6 @@ $(document).ready()
       } else {
         $("span#triangle-id").text("Not a triangle!");
       }
+    $("div.output").show();
+  });
 });
